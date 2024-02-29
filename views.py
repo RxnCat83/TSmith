f9aefc77e8a5b95326e01cc20c006dd91598eb1e
RxnCat83 <m.marc.smith@protonmail.com>
2024-02-29 20:26:49 -0300

    registration

--- views.py
+++ views.py
@@ -39,3 +39,6 @@ def logout_user(request):
     logout(request)
     messages.success(request, "You have successfully logged out.")
     return redirect('home')
+
+def register_user(request):
+    return render(request, 'register.html', {})
